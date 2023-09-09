d47f9f0826c22201cbb61d6ada938e608df546f5
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-09 17:36:22 +0530

    added new codesnipt

--- Obserbable.js
+++ Obserbable.js
@@ -8,14 +8,12 @@ let Utility = {
     }
 }
 
-/**
- * @class Observable
- */
+/** @class Observable */
 var Observable = (function() {
 
     /**
      * @class Subscription
-     * @param {function(V): void} onUpdate 
+     * @param {(value: V) => void} onUpdate 
      */
     function Subscription(onUpdate) {
         this._id = Utility.createId();
@@ -23,35 +21,32 @@ var Observable = (function() {
     }
 
 
-    /**
-     * @type {V}
-     */
+    /** @type {V} */
     let data;
 
-    /**
-     * @type {Array<Subscription>}
-     */
+    /** @type {Array<Subscription>} */
     let subscribers = [];
 
     /**
      * @constructor Observable
-     * @param {V} [init_val]  initial Value
+     * @param {V} [initVal] initial Value
      * @returns {Observable}
      * @template V
      */
-    function Observable(init_val) {
+    function Observable(initVal) {
         if(!new.target) throw new Error('This is a Class. Use `new` keyword to create Object.');
-        if(!Utility.isNone(init_val)) data = init_val
+        if(!Utility.isNone(initVal)) data = initVal;
     }
 
     /**
      * @method subscribe
-     * @param {function(V): void} onUpdate 
+     * @param {(value: V) => void} onUpdate 
      */
     Observable.prototype.subscribe = function(onUpdate) {
         subscribers.push(new Subscription(onUpdate));
     }
 
+    /** @private */
     const updateSubscribers = () => {
         for (let i=0, l=subscribers.length; i<l; i++) {
             queueMicrotask(() => {subscribers[i].onUpdate(data)});
@@ -69,4 +64,18 @@ var Observable = (function() {
     }
 
     return Observable;
+})();
+
+// use of Observable
+
+let observableData = new Observable(0);
+observableData.subscribe((data) => {
+    console.log(data);
+});
+console.log("data Subscribed");
+
+(function() {
+    ([1,2,5,3,9,6,1,54,5]).forEach((val)=>{
+        observableData.update(val)
+    });
 })();
